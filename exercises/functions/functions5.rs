05b4009f46795bb3fc456443a1913477c176c96f
harry-hrz <haoruizhang.cs@gmail.com>
2023-09-29 14:25:58 +0800

    update

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,7 +1,8 @@
 // functions5.rs
-// Execute `rustlings hint functions5` or use the `hint` watch subcommand for a hint.
+//
+// Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
+// hint.
 
-// I AM NOT DONE
 
 fn main() {
     let answer = square(3);
@@ -9,5 +10,5 @@ fn main() {
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    num * num
 }
--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -1,9 +1,11 @@
 // intro2.rs
+//
 // Make the code print a greeting to the world.
-// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
+//
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
+// hint.
 
-// I AM NOT DONE
 
 fn main() {
-    println!("Hello {}!");
+    println!("Hello World!");
 }
